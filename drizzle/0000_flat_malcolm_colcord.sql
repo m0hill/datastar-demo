c04f33ab9d587cb05dbaf1d6e21923cf7b9cd896
Mohil Garg <112541468+mohilcode@users.noreply.github.com>
2025-07-01 21:03:20 +0900

    ref: migrations

--- drizzle/0000_flat_malcolm_colcord.sql
+++ drizzle/0000_flat_malcolm_colcord.sql
@@ -1,5 +1,5 @@
 CREATE TABLE `memos` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`content` text NOT NULL,
-	`created_at` integer DEFAULT '"2025-06-30T13:12:16.361Z"' NOT NULL
+	`created_at` integer DEFAULT '"2025-07-01T12:02:24.470Z"' NOT NULL
 );
